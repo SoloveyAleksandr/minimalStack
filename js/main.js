7c0b047e8fc10b8e1b72fa90be4b90a9af54bebb
hyper-design <hyper-design@keramin.int>
2021-10-20 11:06:46 +0300

    add-key

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menuBox = document.querySelector('.menu__box');
 
+const arrowBtns = document.querySelectorAll('.cube-buttons__btn');
+const arrowUp = document.querySelector('.arrow-up');
+const arrowLeft = document.querySelector('.arrow-left');
+const arrowDown = document.querySelector('.arrow-down');
+const arrowRight = document.querySelector('.arrow-right');
+
+
 menuBtn.addEventListener('click', (e) => {
     menuBtn.classList.toggle('active');
     menuBox.classList.toggle('active');
@@ -13,10 +20,22 @@ const allSides = document.querySelectorAll('.side');
 
 document.onkeydown = function (e) {
     if (e.key.includes('Arrow')) {
-        if (e.key === 'ArrowLeft') rotateY -= 2
-        else if (e.key === 'ArrowUp') rotateX -= 2
-        else if (e.key === 'ArrowRight') rotateY += 2
-        else if (e.key === 'ArrowDown') rotateX += 2
+        if (e.key === 'ArrowLeft') {
+            rotateY -= 2;
+            arrowLeft.classList.add('active');
+        }
+        else if (e.key === 'ArrowUp') {
+            rotateX -= 2;
+            arrowUp.classList.add('active');
+        }
+        else if (e.key === 'ArrowRight') {
+            rotateY += 2;
+            arrowRight.classList.add('active');
+        }
+        else if (e.key === 'ArrowDown') {
+            rotateX += 2;
+            arrowDown.classList.add('active');
+        }
 
         allSides.forEach(side => {
             side.classList.add('active');
@@ -31,6 +50,9 @@ document.onkeyup = function (e) {
     if (e.key.includes('Arrow')) {
         allSides.forEach(side => {
             side.classList.remove('active')
+        })
+        arrowBtns.forEach(btn => {
+            btn.classList.remove('active');
         })
     }
 };
